a11f8fa4a50f6131ec12a7a373eafe832154cc1b
Ecsedi Milan <milan200314@gmail.com>
2022-09-28 18:33:03 +0200

    feladat 9
     -A ciklus működik
     -Az elágazás megvan határozva

---
+++ src/feladat9.java
@@ -0,0 +1,22 @@
+public class feladat9 {
+
+    public static void main(String[] args) {
+
+
+        for (int i=0; i<=1000; i++){
+
+            if (i % 3 == 0 && i % 5 ==0){
+
+                System.out.print(i+ "; ");
+
+
+            }
+
+
+        }
+
+
+    }
+
+
+}
