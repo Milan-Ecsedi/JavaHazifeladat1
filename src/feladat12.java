6023559204ead5a861e00429cd2bc96a29d8d3bd
Ecsedi Milan <milan200314@gmail.com>
2022-09-28 19:55:51 +0200

    feladat 12
    - az összeget elosztja mindegyik érmével

---
+++ src/feladat12.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class feladat12 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.printf("Adjon meg egy összeget Forintban [1-1000Ft] :");
+
+        int osszeg= sc.nextInt();
+
+        int ft200= osszeg/200;
+        int ft100= osszeg/100;
+        int ft50= osszeg/50;
+        int ft20 =osszeg/20;
+        int ft10 =osszeg/10;
+        int ft5= osszeg/5;
+
+
+
+
+    }
+
+
+
+
+
+}
